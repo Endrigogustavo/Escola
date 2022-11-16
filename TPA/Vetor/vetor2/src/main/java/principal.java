60e2135461f6f64bfb71356d2a17cb3dd15f901e
Endrigogustavo <endrigogustavo2006@gmail.com>
2022-11-16 15:03:30 -0300

    commit

--- TPA/Vetor/vetor2/src/main/java/principal.java
+++ TPA/Vetor/vetor2/src/main/java/principal.java
@@ -2,15 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package vetorteste.vetor;
 
 /**
  *
  * @author dti
  */
-public class Vetor {
-    public static void main(String[] args) {
-        System.out.println("pp");
-    }
+public class principal {
     
 }
